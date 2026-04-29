3d0ff8ef934ab0797b680a45b1ee8a7b09329f65
Ilya Obryvin <ilyaobryvin@gmail.com>
2026-04-29 15:51:27 +0300

    [stepik.org/lesson/1737832/] add dict emp passengers from seed file

--- dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_tickets.sql
+++ dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_tickets.sql
@@ -11,3 +11,4 @@ select
     passenger_name,
     contact_data
 from {{ ref('stg_flights__tickets') }}
+-- where passenger_id not in (select distinct passenger_id from {{ ref('dct_flight__employee_passengers') }})
