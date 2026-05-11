edc104ecc000024758fd9e97daff91ad4e4447f5
Ilya Obryvin <ilyaobryvin@gmail.com>
2026-05-11 16:02:23 +0300

    [stepik.org/lesson/1737854/] generate_surrogate_key

--- dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_bookings.sql
+++ dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_bookings.sql
@@ -6,4 +6,5 @@
 
 select 
     {{ show_columns_relation('stg_flights__bookings') }}
+    , {{ dbt_utils.generate_surrogate_key(['book_ref']) }} as book_ref_key
 from {{ ref('stg_flights__bookings') }}
