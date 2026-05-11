a09ed45953f24d4c189dc1df52983ff4e4320e5a
Ilya Obryvin <ilyaobryvin@gmail.com>
2026-05-11 16:26:55 +0300

    [stepik.org/lesson/1737854/] get_column_values

--- dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_flights.sql
+++ dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_flights.sql
@@ -1,6 +1,21 @@
 {{
     config(
-        materialized='table'
+        materialized='table',
+        post_hook="
+            {% if execute %}
+                {% set statuses = dbt_utils.get_column_values(
+                    table=this,
+                    column='status'
+                ) %}
+                
+                {% do log('Актуальные статусы:', info=true) %}
+                {% for status in statuses %}
+                    {% do log('- ' ~ status, info=true) %}
+                {% endfor %}
+            {% endif %}
+
+            select 1
+        "
     )
 }}
 
