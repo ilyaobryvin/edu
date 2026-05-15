f614df4bbde982953879c94b5154857cac363778
Ilya Obryvin <ilyaobryvin@gmail.com>
2026-05-15 23:11:05 +0300

    [stepik.org/lesson/1747616/] unit test 1

--- dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_flights.sql
+++ dbt_stepik/dbt_course_practice/models/intermediate/flights/fct_flights.sql
@@ -29,5 +29,17 @@ select
     status,
     aircraft_code,
     actual_departure,
-    actual_arrival
+    actual_arrival,
+    
+    case
+        when actual_departure is not null and scheduled_departure < actual_departure
+        then actual_departure - scheduled_departure
+        else INTERVAL '0 seconds'
+    end as flight_departure_delay,
+
+    case
+        when actual_departure is not null and actual_arrival is not null
+        then actual_arrival - actual_departure
+        else INTERVAL '0 seconds'
+    end as actual_duration_flight
 from {{ ref('stg_flights__flights') }}
